5cdc3d217b92a22e25ec9808e4a42dd23ba50862
Alissonf94 <alissonfernandesxx55@gmail.com>
2023-10-28 11:12:43 -0300

    main

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/components/main/index'
+import {Montserrat_700Bold, useFonts  } from "@expo-google-fonts/montserrat";
 
 export default function App() {
+  let [fonsLoad, fontsErro] = useFonts({
+    Montserrat_700Bold,
+})
+
+if(!fontsErro && !fonsLoad){
+  return null
+}
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Main/>
     </View>
   );
 }
@@ -14,7 +22,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });

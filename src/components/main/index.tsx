54c07e331a6f642fd9af8b62717a2a0f766017ef
Miguel Farias <miguelcoelho500@gmail.com>
2023-10-30 09:25:04 -0300

    teste de commit inicial

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,26 +1,33 @@
-import { View, Image, TouchableOpacity, ImageBackground, Text } from "react-native";
-import Styles from '../main/style'
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+  Text,
+} from "react-native";
+import Styles from "../main/style";
 
-export default function Main(){
-    return(
-       
-       <View style={Styles.container}>
-        <ImageBackground style={Styles.background} source={require('../../images/background.png')} resizeMode="cover">
-            <Image source={require('../../images/logo.png')}/>
-            
-            <View style={Styles.buttons}>
-                <TouchableOpacity style={Styles.buttonLogin}>
-                    <Text style={Styles.textButton}>Sign in</Text>
-                </TouchableOpacity>
-                
-                <TouchableOpacity style={Styles.buttonRegister}>
-                    <Text style={Styles.textButton}>Register</Text>
-                </TouchableOpacity>
-                <Text>yfgvkjgkbl</Text>
-            </View>
+export default function Main() {
+  return (
+    <View style={Styles.container}>
+      <ImageBackground
+        style={Styles.background}
+        source={require("../../images/background.png")}
+        resizeMode="cover"
+      >
+        <Image source={require("../../images/logo.png")} />
 
-        </ImageBackground>
-       </View>
-       
-    )
+        <View style={Styles.buttons}>
+          <TouchableOpacity style={Styles.buttonLogin}>
+            <Text style={Styles.textButton}>Sign in</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={Styles.buttonRegister}>
+            <Text style={Styles.textButton}>Register</Text>
+          </TouchableOpacity>
+          <Text>yfgvkjgklhgkhghckjvbl</Text>
+        </View>
+      </ImageBackground>
+    </View>
+  );
 }

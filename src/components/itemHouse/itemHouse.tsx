b824d849a17f290785f59e42207d66d96557510b
Miguel Farias <miguelcoelho500@gmail.com>
2023-11-06 21:22:26 -0300

    fix bugs

--- src/components/itemHouse/itemHouse.tsx
+++ src/components/itemHouse/itemHouse.tsx
@@ -1,23 +1,17 @@
-import Styles from "./styleItemHouse"
-import { House} from '../../Data/ListHouses'
-import {View, Image} from 'react-native'
-import { Text } from "react-native"
+import Styles from "./styleItemHouse";
+import { House } from "../../Data/ListHouses";
+import { View, Image } from "react-native";
+import { Text } from "react-native";
 
-export default function App({name, image, description,price}:House){
-    return(
+export default function App({ name, image, description, price }: House) {
+  return (
     <View style={Styles.container}>
-        <Image style= {Styles.image} source={image}/>
-        <View style={Styles.containerText}>
-            <Text style={Styles.textName}>
-                {name}
-            </Text>
-            <Text  style={Styles.textDescription} >
-                {description}
-            </Text>
-            <Text style={Styles.textPrice}>
-                R$ {price}
-            </Text>
-        </View>
+      <Image style={Styles.image} source={image} />
+      <View style={Styles.containerText}>
+        <Text style={Styles.textName}>{name}</Text>
+        <Text style={Styles.textDescription}>{description}</Text>
+        <Text style={Styles.textPrice}>R$ {price}</Text>
+      </View>
     </View>
-    )
+  );
 }

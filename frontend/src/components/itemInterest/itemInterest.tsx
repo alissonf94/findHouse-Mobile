37aa594e150fbb0e6c11193d83d99c6ef7b92fcb
ArthurFreiredeMorais <arthurfreiredemorais@gmail.com>
2023-12-08 17:03:43 -0300

    interests updated css

--- frontend/src/components/itemInterest/itemInterest.tsx
+++ frontend/src/components/itemInterest/itemInterest.tsx
@@ -1,18 +1,20 @@
 import Styles from "./styleItemInterest";
-import { View, Image, TouchableOpacity } from "react-native";
-import { Text, Button } from "react-native";
+import { View, Image, TouchableOpacity, Text, Button } from "react-native";
 import { Interest } from "../../types/RootStackParamList"
 import Icon from "react-native-vector-icons/AntDesign";
 
 export default function App({_id, name , email, phone, immobile }: Interest) {
   return (
     <View style={Styles.container}>
-      <View style={Styles.containerHouse}>
-        <Text style={Styles.textHouse}>{immobile.name}</Text>
-      </View>
-      <View>
-        <Text style={Styles.textName}>Broker: {immobile.agent}</Text>
-      </View>
+    <View style={Styles.containerHouse}>
+      <Text numberOfLines={3} style={Styles.textHouse}>{immobile.name}</Text>
     </View>
+    <View style={Styles.containerBroker}>
+      <Text numberOfLines={3} style={Styles.textName}>Broker: {immobile.agent}</Text>
+    </View>
+    <TouchableOpacity style={Styles.deleteButton}>
+        <Icon name="delete" size={18} style={Styles.iconDelete}/>
+    </TouchableOpacity>
+  </View>
   );
 }
